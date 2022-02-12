d042e87ef5e268e0b38a90c3899d82f2b1b9c7d2
dashwin82 <78400821+dashwin82@users.noreply.github.com>
2022-02-12 05:37:17 +0530

    Study
    First Push

---
+++ java8/src/com/dashwin82/annotations/OverrideExample.java
@@ -0,0 +1,36 @@
+package com.dashwin82.annotations;
+
+
+class Base {
+
+    // Method
+    public void display()
+    {
+        System.out.println("Base display()");
+    }
+}
+
+// Class 2
+// Main class
+class Derived extends Base {
+
+    // Overriding method as already up in above class
+    @Override public void display()
+    {
+        // Print statement when this method is called
+        System.out.println("Derived display(int )");
+    }
+}
+
+public class OverrideExample {
+
+    public static void main(String[] args)
+    {
+        // Creating object of this class inside main()
+        Base obj = new Derived();
+
+        // Calling display() method inside main()
+        obj.display();
+    }
+}
+
